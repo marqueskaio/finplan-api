9a43f586e296a7c57b66be5d118b8125af8d2fcd
marqueskaio <marques.kaio16@gmail.com>
2023-04-22 18:57:39 -0300

    feat:getAllFaturas, GetAllFaturasUsecase: Should return a fatura with correct values

--- src/usecases/faturas/get-all-faturas/get-all-faturas-usecase.spec.ts
+++ src/usecases/faturas/get-all-faturas/get-all-faturas-usecase.spec.ts
@@ -1,6 +1,6 @@
 import { Bancos, FaturasModel } from "@prisma/client"
 import { GetAllFaturasRepositoryInterface } from "interfaces/repositories/faturas/get-all-faturas-repository-interface"
-import { GetAllFaturasUsecase } from "usecases/faturas/get-all-faturas/get-all-faturas-usecase"
+import { GetAllFaturasUsecase } from "/Users/Public/Documents/finplan-api/src/usecases/faturas/get-all-faturas/get-all-faturas-usecase"
 
 const faturasModel = (): FaturasModel => {
     return {
@@ -14,7 +14,7 @@ const faturasModel = (): FaturasModel => {
 
 
 describe('GetAllFaturasUsecase', ()=>{
-    test('Should return a fatura on success',  ()=>{
+    test('Should return a fatura on success',  async ()=>{
         class GetAllFaturasRepositoryStub implements GetAllFaturasRepositoryInterface{
             async getAll(): Promise<FaturasModel | null> {
                 return Promise.resolve(faturasModel())
@@ -23,12 +23,19 @@ describe('GetAllFaturasUsecase', ()=>{
 
         const getAllFaturasRepositoryStub = new GetAllFaturasRepositoryStub()
         const sut = new GetAllFaturasUsecase(getAllFaturasRepositoryStub)
-        const result = sut.getAll("Janeiro")
-
-        expect(result).toEqual({id: 1,
+        const spy = jest.spyOn(getAllFaturasRepositoryStub, 'getAll')
+        await sut.execute({
             mes_fatura: "Janeiro",
             banco: Bancos.ITAU,
             dia_do_fechamento: new Date(23/4/2023),
-            dia_do_vencimento: new Date(30/4/2023)})
+            dia_do_vencimento: new Date(30/4/2023)
+        })
+
+        expect(spy).toHaveBeenCalledWith({
+            mes_fatura: "Janeiro",
+            banco: Bancos.ITAU,
+            dia_do_fechamento: new Date(23/4/2023),
+            dia_do_vencimento: new Date(30/4/2023)
+        })
     });
 })
